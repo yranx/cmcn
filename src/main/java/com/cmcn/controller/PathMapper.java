519ee19fa446c7a72b5686a3db7e2807a66f4467
ranx <yranforever@163.com>
2018-12-12 16:07:15 +0800

    mark

---
+++ src/main/java/com/cmcn/controller/PathMapper.java
@@ -0,0 +1,19 @@
+package com.cmcn.controller;
+
+import io.netty.channel.ChannelHandlerContext;
+import net.sf.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 请求业务转发
+ * @author ranx
+ * @create 2018-11-10 20:26
+ **/
+public class PathMapper {
+    private static Logger logger = LoggerFactory.getLogger(PathMapper.class);
+
+    public void handler(String path, JSONObject params, ChannelHandlerContext ctx) {
+
+    }
+}
